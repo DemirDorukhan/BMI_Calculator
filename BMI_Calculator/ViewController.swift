54bb365fb5a1aa686c9d849637cedf1b432725a6
Dorukhan Demir <79447326+DemirDorukhan@users.noreply.github.com>
2024-03-08 16:40:05 +0300

    Initial Commit

---
+++ BMI_Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI_Calculator
+//
+//  Created by Dorukhan Demir on 8.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
